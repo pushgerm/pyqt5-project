92af51e7bc57a838532ce6ae98f4c9aef6f86407
brandon <mkk0618@naver.com>
2018-03-17 21:59:21 +0900

    x axe changed. only FFT left

--- pushgerm/QMainWindow.py
+++ pushgerm/QMainWindow.py
@@ -13,47 +13,47 @@ class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.left = 100
-        self.top = 100
-        self.title = 'FFT project'
-        self.width = 1500
-        self.height = 800
+        self.left = 100#윈도우창 x축 위치
+        self.top = 100#윈도우창 y축 위치
+        self.title = 'FFT project'#윈도우창 이름
+        self.width = 1500#윈도우창 너비
+        self.height = 800#윈도우창 높이
 
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('File')
+        mainMenu = self.menuBar()#메뉴바 생성
+        fileMenu = mainMenu.addMenu('File')#메뉴바에 File 메뉴 추가
 
-        loadButton = QAction('load', self)
-        loadButton.triggered.connect(self.loadData)
+        loadSub = QAction('load', self)#File메뉴에 sub메뉴 생성
+        loadSub.triggered.connect(self.loadData)#누르면 데이터 불러오기 생성
 
-        exitButton = QAction(QIcon('a.png'), 'Exit', self)
-        exitButton.triggered.connect(self.close)
-        fileMenu.addAction(loadButton)
+        exitButton = QAction(QIcon('a.png'), 'Exit', self)#exit
+        exitButton.triggered.connect(self.close)#누르면 나가짐
+        fileMenu.addAction(loadSub)
         fileMenu.addAction(exitButton)
 
         self.initUI()
 
 
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle(self.title)#윈도우창 타이틀
+        self.setGeometry(self.left, self.top, self.width, self.height)#전체 윈도우 창 위치 및 크기
 
-        label = QLabel(self)
-        pixmap = QPixmap('image.JPG')
-        label.setPixmap(pixmap)
-        label.move(34, 50)
-        label.resize(638, 410)
+        label = QLabel(self)#사진 넣을 공간 마련
+        pixmap = QPixmap('image.JPG') # 사진 불러오기
+        label.setPixmap(pixmap)# 레이블에 사진 삽입
+        label.move(34, 50)#사진위치조정
+        label.resize(638, 410)#사진 크기조정(실제사이즈)
 
-        btn1 = QPushButton('Load Data', self)
-        btn1.move(250, 550)
-        btn1.resize(200, 50)
+        btn1 = QPushButton('Load Data', self)#데이터 불러오기 버튼
+        btn1.move(250, 550)#버튼 위치
+        btn1.resize(200, 50)#버튼 크기
         btn1.clicked.connect(self.loadData)  #클릭시 csv파일 선택해 열기
 
-        btn2 = QPushButton('Transform', self)
-        btn2.move(250, 650)
-        btn2.resize(200, 50)
+        btn2 = QPushButton('Transform', self)#FFT변환 버튼
+        btn2.move(250, 650)#버튼 위치
+        btn2.resize(200, 50)#버튼 크기
 
-        self.m = PlotCanvas(self, width=8, height=8)
-        self.m.move(700, 0)
+        self.m = PlotCanvas(self, width=8, height=8)#윈도우창에 그래프 삽입
+        self.m.move(700, 0)#그래프 위치
 
         self.show()
 
@@ -61,17 +61,23 @@ class App(QMainWindow):
         fname = QFileDialog.getOpenFileName(self)[0]
         file_extension = fname.split('.')[-1]
 
-        self.canOpenData = True
+        # .csv파일이 아니면 에러창 띄움
+        if file_extension != "csv":
+            PopUpWindow()
+            return
+
         input = np.loadtxt(fname, delimiter = ',', dtype = np.float32)
-        print(fname[-8:-4])
+        ndata = input.shape[0]
         if(fname[-7:-4] =="acc"):
-            self.time = input[:, 6:7]
-            self.x = input[:, 4:5]
-            self.m.plot_acc(self.time, self.x)
+            #self.time = input[:, 6:7]#시간데이터를 새로 만들었음 기존 usec을 쓰면 데이터가 겹침
+            self.time = np.array([[x] for x in range(1, ndata + 1)])
+            self.x = input[:, 4:5]#x가속도 데이터
+            self.y = input[:, 5:6]#y가속도 데이터
+            self.m.plot_acc(self.time, self.x, self.y)#acc.csv그래프 그리기
         elif(fname[-8:-4]=="temp"):
-            self.time = input[:, 5:6]
-            self.x = input[:, 4:5]
-            self.m.plot_temp(self.time, self.x)
+            self.time = input[:, 5:6]#시간데이터를 새로만듦
+            self.x = input[:, 4:5]#온도
+            self.m.plot_temp(self.time, self.x)#temp.csv그래프 그리기
 
 class PlotCanvas(FigureCanvas):
 
@@ -79,25 +85,41 @@ class PlotCanvas(FigureCanvas):
         fig = Figure(figsize=(width, height), dpi=dpi)
 
         FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        self.ax1 = self.figure.add_subplot(211)
-        self.ax2 = self.figure.add_subplot(212)
+        self.setParent(parent)#what does this instruction mean?
+        self.ax1 = self.figure.add_subplot(211)# make a subplot1
+        self.ax2 = self.figure.add_subplot(212)# make a subplot 2
 
-        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+        #FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        #FigureCanvas.updateGeometry(self)
 
-    def plot_acc(self, time1, x):
+    def plot_acc(self, time1, x, y):
 
-        self.ax1.plot(time1, x, label = 'x acceleration')
-        #self.ax1.plot(time1, y, label = 'y acceleration')
-        self.ax1.hold(False)
+        self.ax1.plot(time1, x, label='x acceleration')#why doesn't the label show up?
+        self.ax1.plot(time1, y, label = 'y acceleration')
+        self.ax1.set_xlabel('time')#name of x axe
+        self.ax1.set_ylabel('acceleration')#name of y axe
+        self.ax1.hold(False)#data reset when the new data come
         self.draw()
 
     def plot_temp(self, time, x):
         self.ax1.plot(time, x, label = 'temp')
-        self.ax1.hold(False)
+        self.ax1.set_xlabel('time')#name of x axe
+        self.ax1.set_ylabel('temperature')#name of y axe
+        self.ax1.hold(False)#data reset when the new data come
         self.draw()
 
+class PopUpWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        fg = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        fg.moveCenter(cp)
+        self.move(fg.topLeft())
+
+        buttonReply = QMessageBox.question(self, "Error", "Failed to load data.\ncsv file (.csv) only", QMessageBox.Yes)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
